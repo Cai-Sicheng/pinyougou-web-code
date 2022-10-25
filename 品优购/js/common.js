5c4849ec8aac49b0c1d2828891229cfa231d5252
Cai-Sicheng <100366414+Cai-Sicheng@users.noreply.github.com>
2022-10-25 20:01:32 +0800

    Update common.js

--- 品优购/js/common.js
+++ 品优购/js/common.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', function()  {
 var dropdown=document.querySelector('.dropdown')
     var dd=document.querySelector('.dd')
       dropdown.onmouseover = function() {
-          dd.style.display='block
+          dd.style.display='block'
         }
         dropdown.onmouseout = function() {
           dd.style.display='none'
