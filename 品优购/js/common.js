7ee71049acfeb6728a612466b47cb77a38d33816
Cai-Sicheng <100366414+Cai-Sicheng@users.noreply.github.com>
2022-10-25 19:58:35 +0800

    Dom加载操作

--- 品优购/js/common.js
+++ 品优购/js/common.js
@@ -1,12 +1,10 @@
-window.onload=function(){
-    var dropdown=document.querySelector('.dropdown')
+window.addEventListener('DOMContentLoaded', function()  {
+var dropdown=document.querySelector('.dropdown')
     var dd=document.querySelector('.dd')
       dropdown.onmouseover = function() {
-          dd.style.display='block'
-      
+          dd.style.display='block
         }
         dropdown.onmouseout = function() {
           dd.style.display='none'
         }
-
-  } 
+})
